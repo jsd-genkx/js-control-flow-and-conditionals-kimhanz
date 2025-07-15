aab78710f2c3313b8cf2d07c519a53a163807e21
Kim Hanz <nanaka3@hotmail.com>
2025-07-15 11:40:55 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -1,5 +1,11 @@
-"use strict"
+"use strict";
 
 // TODO: Use if/else to print "Even" or "Odd"
 
-const number = 15;
+const number = 13;
+
+if (number % 2 === 0) {
+  console.log(`${number} is Even`);
+} else {
+  console.log(`${number} is Odd`);
+}
